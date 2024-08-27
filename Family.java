5b26cdb8a134d7f39b374e9184a0e921ec5819e2
surendranathbe <162865290+surendranathbe@users.noreply.github.com>
2024-08-27 20:43:06 +0530

    Add files via upload

---
+++ Family.java
@@ -0,0 +1,10 @@
+
+public class Family {
+    public static void main(String[] args) {
+        System.out.println("bezawada subbarao");
+        System.out.println("bezawada sridevi");
+        System.out.println("bezawada vishnu vardhan");
+        System.out.println("bezawada surendra nath");
+    }
+
+}
